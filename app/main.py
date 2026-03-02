f347e68834a2582756d66149d769436839bd2e49
SebastianOG509 <seortizg@unal.edu.co>
2026-03-02 15:05:52 -0500

    Refactor estructura de archivos y funciones de base de datos
    - Eliminar archivo de usuarios y mover la lógica de la base de datos a session.py.
    - Agregar función get_db para manejar sesiones de base de datos.
    - Actualizar el enrutador de usuarios y la ruta de salud para usar la nueva función de base de datos.
    - Reubicar la validación de JWT a un nuevo archivo en la estructura de seguridad.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
-from fastapi import FastAPI
-from app.api.v1 import users
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+from app.modules.users.router import router as users_router
+from app.db.session import get_db
 
 app = FastAPI(
-    title="Performance Evaluation Platform",
+    title="Plataforma de Evaluación de Productividad",
     version="1.0.0"
 )
 
-app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
+app.include_router(users_router, prefix="/users", tags=["Users"])
+
 
 @app.get("/health")
-def health():
-    return {"status": "ok"}
+def health(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+        return {
+            "status": "ok",
+            "database": "connected"
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "database": "disconnected",
+            "detail": str(e)
+        }
