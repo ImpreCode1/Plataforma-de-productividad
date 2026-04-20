021ab292b8f363a1cc8efc68cb5d2c4a67589812
SebastianOG509 <seortizg@unal.edu.co>
2026-04-20 14:28:26 -0500

    Actualizar configuración para cargar JWT_SECRET desde el entorno y mejorar la impresión de depuración.

--- app/core/config.py
+++ app/core/config.py
@@ -3,11 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print(f"🔍 .env loaded, JWT_SECRET from env: {os.getenv('PUBLIC_KEY_OR_SECRET')}")
+
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL") # type: ignore
-    JWT_SECRET: str = "super_secret_key"
+    DATABASE_URL: str = os.getenv("DATABASE_URL") #type: ignore
+    JWT_SECRET: str = os.getenv("PUBLIC_KEY_OR_SECRET") #type: ignore
     JWT_ALGORITHM: str = "HS256"
     JWT_ISSUER: str = "hydra-iam"
     JWT_AUDIENCE: str = "internal-platforms"
 
 settings = Settings()
+print(f"🔍 settings.JWT_SECRET: {settings.JWT_SECRET}")
